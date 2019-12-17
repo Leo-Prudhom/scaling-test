5ae8141f0d68a47a5bc941cba6df0a572e869fc1
leo-prud <leo.prudh@gmail.com>
2019-12-17 21:54:36 +0100

    first commit test

---
+++ components/HelloText.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class HelloText extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HELLo hello</Text>
+      </View>
+    );
+  }
+}
